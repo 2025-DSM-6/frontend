3c36f3546fc062d96bccecf4e2952433ef84ad82
heedda <heedda.kollim@gmail.com>
2025-07-17 10:48:32 +0900

    로그인 서버 연동

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,8 +1,24 @@
 import useLoginInputStore from "@/store/loginInputStore";
+import api from "@/apis/instance.js";
 import * as S from "./styles";
 
 function LoginPage() {
   const { username, password, setUsername, setPassword } = useLoginInputStore();
+
+  const handleLogin = async () => {
+    try {
+      const response = await api.post("/auth", {
+        account_id: username,
+        password: password,
+      });
+      console.log("로그인 성공:", response.data);
+      document.cookie = `accessToken=${response.data.access_token}; path=/;`;
+      window.location.href = "/";
+    } catch (error) {
+      console.error("로그인 실패:", error.response?.data || error.message);
+    }
+  };
+
   return (
     <S.Background>
       <S.LoginBox>
@@ -31,7 +47,9 @@ function LoginPage() {
             </S.Label>
           </S.InputSection>
         </form>
-        <S.Button onClick={() => { }}>로그인</S.Button>
+        <S.Button onClick={handleLogin}>
+          로그인
+        </S.Button>
       </S.LoginBox>
     </S.Background>
   );
